92b4c9527406e1a5d4f28a82dfe5661cec6b6f84
Wonkeun No <zakk23v@hotmail.com>
2023-01-22 01:03:34 -0500

    Update register route with User schema

--- src/authorization/index.js
+++ src/authorization/index.js
@@ -1,41 +1,66 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  onAuthStateChanged,
 } = require('firebase/auth');
-const { app } = require('../firebase/config');
+const { collection, addDoc } = require('firebase/firestore');
+const { db } = require('../firebase/config');
+const { User } = require('../model/User');
 
 const auth = getAuth();
 
 router.post('/register', (req, res) => {
-  console.log(`REQ: ${req.body.password}`);
-  createUserWithEmailAndPassword(auth, req.body.email, req.body.password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      res.status(200).json({ code: 201, message: `${req.body.email} has been registered` });
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
+  if (req.body.password1 !== req.body.password2) {
+    res.status(401).json({ message: 'Passwords do not match', code: 401 });
+  } else {
+    bcrypt
+      .hash(req.body.password1, 10)
+      .then((hash) => {
+        createUserWithEmailAndPassword(auth, req.body.email, hash)
+          .then(async (userCredential) => {
+            const ref = collection(db, 'users').withConverter(userConverter);
+            await addDoc(ref, new User(userCredential.user.uid, req.body.email, hash));
+            res.status(201).json({ message: `${req.body.email} has been registered`, code: 201 });
+          })
+          .catch((error) => {
+            const errorMessage = error.message;
+            res.status(500).json({
+              message: errorMessage,
+              code: 500,
+            });
+          });
+      })
+      .catch((err) => {
+        res.status(500).json({ message: 'Something went wrong while hashing password', code: 500 });
+      });
+  }
 });
 
 router.post('/login', (req, res) => {
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in
-      const user = userCredential.user;
-      // ...
     })
     .catch((error) => {
-      const errorCode = error.code;
       const errorMessage = error.message;
     });
 });
+
+const userConverter = {
+  toFirestore: (user) => {
+    return {
+      id: user.id,
+      email: user.email,
+      password: user.password,
+    };
+  },
+  fromFirestore: (snapshot, options) => {
+    const data = snapshot.data(options);
+    return new User(data.id, data.email, data.password);
+  },
+};
 
 module.exports = router;
