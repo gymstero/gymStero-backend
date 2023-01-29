d75acdb4031901363ccb617bf18fd7853c22746b
ArazTey <ateymourzadeh@myseneca.ca>
2023-01-29 01:40:10 -0500

    login and logout functionality added

--- src/authorization/index.js
+++ src/authorization/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
-
 const {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  signOut,
 } = require('firebase/auth');
-const { collection, addDoc, getDocs, query, where } = require('firebase/firestore');
+const {
+  collection,
+  addDoc,
+  query,
+  where,
+  limit,
+  getDocs,
+  getCountFromServer,
+} = require('firebase/firestore');
 const { db } = require('../firebase/config');
 const { User } = require('../model/User');
 
@@ -26,7 +32,7 @@ router.post('/register', async (req, res) => {
         createUserWithEmailAndPassword(auth, email, hash)
           .then(async (userCredential) => {
             const ref = collection(db, 'users').withConverter(userConverter);
-            await addDoc(ref, new User(userCredential.user.uid, email, hash));
+            await addDoc(ref, new User(userCredential.user.uid, email, hash, ''));
             res.status(201).json({ message: `${email} has been registered`, code: 201 });
           })
           .catch((error) => {
@@ -42,6 +48,40 @@ router.post('/register', async (req, res) => {
       });
   }
 });
+
+router.post('/register-with-google', async (req, res) => {
+  const { uid, email, photoURL } = req.body;
+  try {
+    const q = query(collection(db, 'users'), where('email', '==', email), limit(1));
+    const snapshot = await getCountFromServer(q);
+
+    if (snapshot.data().count > 0) {
+      console.log(`User ${email} already exists`);
+    } else {
+      const ref = collection(db, 'users').withConverter(userConverter);
+      await addDoc(ref, new User(uid, email, '', photoURL));
+      console.log(`User ${email} stored in Firestore`);
+    }
+  } catch (err) {
+    console.error('Something went wrong while storing user info in Firestore', err);
+  }
+});
+
+const userConverter = {
+  toFirestore: (user) => {
+    return {
+      id: user.id,
+      email: user.email,
+      password: user.password,
+      photoURL: user.photoURL,
+    };
+  },
+  fromFirestore: (snapshot, options) => {
+    const data = snapshot.data(options);
+    return new User(data.id, data.email, data.password, data.photoURL);
+  },
+};
+
 /*---------------------------------------------------*/
 //login
 router.post('/login', async (req, res) => {
@@ -76,7 +116,6 @@ router.post('/logout', (req, res) => {
     .signOut()
     .then(() => {
       res.json({ message: 'Log out successful!' });
-      res.redirect('/login');
     })
     .catch((error) => {
       res.status(500).json({ error: error.message });
@@ -84,18 +123,5 @@ router.post('/logout', (req, res) => {
 });
 
 /*---------------------------------------------------*/
-const userConverter = {
-  toFirestore: (user) => {
-    return {
-      id: user.id,
-      email: user.email,
-      password: user.password,
-    };
-  },
-  fromFirestore: (snapshot, options) => {
-    const data = snapshot.data(options);
-    return new User(data.id, data.email, data.password);
-  },
-};
 
 module.exports = router;
