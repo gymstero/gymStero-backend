fe6488b8a85fa46e9b236fab8f0e2956956497e8
Wonkeun No <zakk23v@hotmail.com>
2023-01-30 00:06:02 -0500

    Add status code to response

--- src/authorization/index.js
+++ src/authorization/index.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
-const { getDocs } = require('firebase/firestore');
 const {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
 } = require('firebase/auth');
-const {
-  collection,
-  addDoc,
-  query,
-  where,
-  limit,
-  getCountFromServer,
-} = require('firebase/firestore');
+const { collection, addDoc, getDocs, query, where } = require('firebase/firestore');
 const { db } = require('../firebase/config');
 const { User } = require('../model/User');
 
@@ -32,7 +25,7 @@ router.post('/register', async (req, res) => {
         createUserWithEmailAndPassword(auth, email, hash)
           .then(async (userCredential) => {
             const ref = collection(db, 'users').withConverter(userConverter);
-            await addDoc(ref, new User(userCredential.user.uid, email, hash, ''));
+            await addDoc(ref, new User(userCredential.user.uid, email, hash));
             res.status(201).json({ message: `${email} has been registered`, code: 201 });
           })
           .catch((error) => {
@@ -48,49 +41,13 @@ router.post('/register', async (req, res) => {
       });
   }
 });
-
-router.post('/register-with-google', async (req, res) => {
-  const { uid, email, photoURL } = req.body;
-  try {
-    const q = query(collection(db, 'users'), where('email', '==', email), limit(1));
-    const snapshot = await getCountFromServer(q);
-
-    if (snapshot.data().count > 0) {
-      console.log(`User ${email} already exists`);
-    } else {
-      const ref = collection(db, 'users').withConverter(userConverter);
-      await addDoc(ref, new User(uid, email, '', photoURL));
-      console.log(`User ${email} stored in Firestore`);
-    }
-  } catch (err) {
-    console.error('Something went wrong while storing user info in Firestore', err);
-  }
-});
-
-const userConverter = {
-  toFirestore: (user) => {
-    return {
-      id: user.id,
-      email: user.email,
-      password: user.password,
-      photoURL: user.photoURL,
-    };
-  },
-  fromFirestore: (snapshot, options) => {
-    const data = snapshot.data(options);
-    return new User(data.id, data.email, data.password, data.photoURL);
-  },
-};
-
-/*---------------------------------------------------*/
-//login
 router.post('/login', async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   try {
     const userCheck = await getDocs(query(collection(db, 'users'), where('email', '==', email)));
     if (userCheck.size !== 1) {
-      return res.status(404).json({ message: 'User Does Not Exist' });
+      return res.status(404).json({ code: 404, message: 'User Does Not Exist' });
     }
     let hashedPassword;
     userCheck.forEach((user) => {
@@ -99,29 +56,41 @@ router.post('/login', async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, hashedPassword);
     if (!isMatch) {
-      return res.status(401).json({ message: 'Wrong Password' });
-    } else {
-      await signInWithEmailAndPassword(auth, email, hashedPassword);
-      res.json({ message: 'Login successful!' });
-    }
+        return res.status(401).json({ code: 401, message: 'Wrong Password' });
+      } else {
+        await signInWithEmailAndPassword(auth, email, hashedPassword);
+        res.status(200).json({ code: 200, message: 'Login successful!' });
+      }
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ code: 500, error: error.message });
   }
 });
 
-/*------------------------------------*/
-//log out
 router.post('/logout', (req, res) => {
   auth
     .signOut()
     .then(() => {
-      res.json({ message: 'Log out successful!' });
+      res.status(200).json({ code: 200, message: 'Log out successful!' });
+      res.redirect('/login');
     })
     .catch((error) => {
-      res.status(500).json({ error: error.message });
+      res.status(500).json({ code: 500, error: error.message });
     });
 });
 
 /*---------------------------------------------------*/
+const userConverter = {
+  toFirestore: (user) => {
+    return {
+      id: user.id,
+      email: user.email,
+      password: user.password,
+    };
+  },
+  fromFirestore: (snapshot, options) => {
+    const data = snapshot.data(options);
+    return new User(data.id, data.email, data.password);
+  },
+};
 
 module.exports = router;
