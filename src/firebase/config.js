010449f43b53ab4b91b05164e340bca2d34ffc29
Wonkeun No <zakk23v@hotmail.com>
2023-04-01 16:36:03 -0400

    Replace serviceKey json with env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,6 @@
 const { initializeApp } = require('firebase/app');
 const { getFirestore } = require('firebase/firestore');
 const admin = require('firebase-admin');
-const serviceAccount = require('../../serviceAccountKey.json');
 require('dotenv').config();
 
 const firebaseConfig = {
@@ -13,7 +12,18 @@ const firebaseConfig = {
   messagingSenderId: process.env.MESSAGE_SENDER_ID,
   appId: process.env.APP_ID,
   measurementId: process.env.MEASUREMENT_ID,
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.SERVICE_ACCOUNT_TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY,
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  }),
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
